f7f4cb9298514059ecbe95176c4387356ad5413b
Al-Qassim <qassimali624@gmail.com>
2025-04-29 15:54:52 +0300

    make CatchDataMemoryRepository

--- src/test/kotlin/data/catchData/CatchDataMemoryRepositoryTest.kt
+++ src/test/kotlin/data/catchData/CatchDataMemoryRepositoryTest.kt
@@ -4,6 +4,12 @@ import logic.repositories.CatchDataRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import com.google.common.truth.Truth.assertThat
+import logic.entities.Project
+import logic.entities.State
+import logic.entities.Task
+import logic.entities.User
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class CatchDataMemoryRepositoryTest {
     private lateinit var catchDataRepository: CatchDataRepository
@@ -12,19 +18,44 @@ class CatchDataMemoryRepositoryTest {
     fun setup() {
         catchDataRepository = CatchDataMemoryRepository()
     }
-//
+
 //    @Test
-//    fun `getLoggedInUser should return null when user is not selected`() {
+//    fun `getLoggedInUser should return null when user is didn't log in`() {
 //        val result = catchDataRepository.getLoggedInUser()
 //
 //        assertThat(result).isEqualTo(null)
 //    }
 //
 //    @Test
-//    fun `getLoggedInUser should return null when user is not selected`() {
+//    fun `getLoggedInUser should return the stored selected user when user have been selected`() {
+//        catchDataRepository.setLoggedInUser(fakeUser)
+//
+//        val result = catchDataRepository.getLoggedInUser()
+//
+//        assertThat(result).isEqualTo(fakeUser)
+//    }
+//
+//    @Test
+//    fun `clearLoggedInUserFromCatch should set the stored user to null` {
+//        catchDataRepository.clearLoggedInUserFromCatch()
+//
 //        val result = catchDataRepository.getLoggedInUser()
 //
 //        assertThat(result).isEqualTo(null)
+//    }
+//
+//    private companion object {
+//        val fakeUser = User(userName = "fake admin user", type = User.Type.ADMIN)
+//        val fakeTask = Task(title = "fake task", description = "no description")
+//        val fakeState = State(title = "TO-DO", description = "")
+//        val fakeProject = Project(
+//            title = "fake project",
+//            description = "",
+//            tasks = listOf(fakeTask),
+//            states = listOf(fakeState)
+//        )
+//        @JvmStatic
+//        fun fakeUsers() = listOf(null, fakeUser)
 //    }
 
 }
