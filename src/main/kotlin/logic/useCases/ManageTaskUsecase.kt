2b67c5d770de5c797209a8c30ebe82aba635b69b
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-04-29 15:06:29 +0300

    add ManageTaskUsecase

---
+++ src/main/kotlin/logic/useCases/ManageTaskUsecase.kt
@@ -0,0 +1,35 @@
+package logic.useCases
+
+import logic.entities.*
+import logic.repositories.TaskRepository
+import logic.repositories.LogsRepository
+import java.time.LocalDateTime
+import java.util.UUID
+
+class ManageTaskUsecase(
+    private val taskRepository: TaskRepository,
+    private val logsRepository: LogsRepository,
+    private val user: User
+) {
+
+    fun addTask(task: Task, projectID: UUID) {
+        taskRepository.addNewTask(task, projectID)
+    }
+
+    fun editTaskTitle(taskID: UUID, newTitle: String) {
+        taskRepository.editTaskTitle(taskID, newTitle)
+    }
+
+    fun editTaskDescription(taskID: UUID, newDescription: String) {
+        taskRepository.editTaskDescription(taskID, newDescription)
+    }
+
+    fun editTaskState(taskID: UUID, newStateID: UUID) {
+        taskRepository.editTaskState(taskID, newStateID)
+    }
+
+    fun deleteTask(taskID: UUID) {
+        taskRepository.deleteTask(taskID)
+    }
+
+}
