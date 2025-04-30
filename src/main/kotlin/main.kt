f2650d54fd5f6d36988245a23c2248b3e55a2ab9
ElhamFadel <elham2000fadel@gmail.com>
2025-04-30 13:00:07 +0300

    implement view for projects managment

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
-fun main() {
+import di.appModule
+import di.presentationModule
+import di.useCasesModule
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
+import ui.projectsView.ProjectsView
 
+fun main() {
+    startKoin {
+        modules(appModule, useCasesModule, presentationModule)
+    }
+    val projectsView: ProjectsView = getKoin().get()
+    projectsView.start()
 }
