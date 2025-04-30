05b26e119467cf954594091442f494670fc69c14
Al-Qassim <qassimali624@gmail.com>
2025-05-01 00:09:41 +0300

    rename CacheDataRepositoryImpl to CacheDataSource

--- src/main/kotlin/di/dataModule.kt
+++ src/main/kotlin/di/dataModule.kt
@@ -1,12 +1,12 @@
 package di
 
 import data.AuthenticationDataSource
-import data.cacheData.CacheDataRepositoryImpl
+import data.cacheData.CacheDataSource
 import logic.repositories.AuthenticationRepository
 import logic.repositories.CacheDataRepository
 import org.koin.dsl.module
 
 val dataModule = module {
     single<AuthenticationRepository> { AuthenticationDataSource() }
-    single<CacheDataRepository> { CacheDataRepositoryImpl() }
+    single<CacheDataRepository> { CacheDataSource() }
 }
