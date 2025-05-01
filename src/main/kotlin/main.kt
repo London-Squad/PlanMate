0706a3c715662a0f402d4694039e8a933a779ed3
pola george <86931053+poula22@users.noreply.github.com>
2025-05-01 17:56:20 +0300

    - add code to create csv files directory

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,11 +4,18 @@ import di.uiModule
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 import ui.welcomeView.WelcomeView
+import java.io.File
 
 fun main() {
     startKoin {
         modules(dataModule, logicModule, uiModule)
     }
+    initFilesDirectory()
     val ui: WelcomeView = getKoin().get()
     ui.startWelcome()
+}
+
+fun initFilesDirectory() {
+    val directory = File("csvFiles")
+    if (!directory.exists()) directory.mkdir()
 }
