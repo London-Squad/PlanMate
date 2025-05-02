abacbed04ca342fc96ba07dde1a6c93807f35204
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-02 15:28:35 +0300

    -add TasksDataSource with tests -fix the task management operations to use the TasksDataSource

--- src/main/kotlin/logic/useCases/ManageTaskUseCase.kt
+++ src/main/kotlin/logic/useCases/ManageTaskUseCase.kt
@@ -1,11 +1,17 @@
 package logic.useCases
 
+import logic.entities.State
+import logic.entities.Task
 import logic.repositories.TaskRepository
 import java.util.UUID
 
 class ManageTaskUseCase(
     private val taskRepository: TaskRepository,
 ) {
+
+    fun addNewTask(task: Task, projectID: UUID) {
+        taskRepository.addNewTask(task, projectID)
+    }
 
     fun editTaskTitle(taskID: UUID, newTitle: String) {
         taskRepository.editTaskTitle(taskID, newTitle)
@@ -15,8 +21,8 @@ class ManageTaskUseCase(
         taskRepository.editTaskDescription(taskID, newDescription)
     }
 
-    fun editTaskState(taskID: UUID, newStateID: UUID) {
-        taskRepository.editTaskState(taskID, newStateID)
+    fun editTaskState(taskID: UUID, newState: State) {
+        taskRepository.editTaskState(taskID, newState)
     }
 
     fun deleteTask(taskID: UUID) {
