153889c37401a78a84ce64d3fb49a119739522cf
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-02 15:31:12 +0300

    -refactor ProjectTasksView to use ManageTaskUseCase -add CsvStatesDataSource skeleton

---
+++ src/main/kotlin/data/dataSource/CsvStatesDataSource.kt
@@ -0,0 +1,33 @@
+package data.dataSource
+
+import logic.entities.State
+import logic.repositories.StatesRepository
+import java.util.*
+
+class CsvStatesDataSource(): StatesRepository {
+    override fun getAllStatesByProjectId(projectId: UUID): List<State> {
+//        TODO("Not yet implemented")
+        return emptyList()
+    }
+
+    override fun getStateById(stateId: UUID): State {
+//        TODO("Not yet implemented")
+        return State.NoState
+    }
+
+    override fun addNewState(state: State, projectId: UUID) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editStateTitle(stateId: UUID, newTitle: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun editStateDescription(stateId: UUID, newDescription: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteState(stateId: UUID) {
+        TODO("Not yet implemented")
+    }
+}
