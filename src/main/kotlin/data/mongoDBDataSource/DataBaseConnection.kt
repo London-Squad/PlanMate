cd05e8d988b0bb8c5d87edef6a3cb542b369d36f
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-05 13:41:59 +0300

    -add mongoDB DataBaseConnection

---
+++ src/main/kotlin/data/mongoDBDataSource/DataBaseConnection.kt
@@ -0,0 +1,26 @@
+package data.mongoDBDataSource
+
+import com.mongodb.reactivestreams.client.MongoClients
+import com.mongodb.reactivestreams.client.MongoCollection
+import com.mongodb.reactivestreams.client.MongoDatabase
+import org.bson.Document
+
+object DataBaseConnection {
+    private const val dbName = "LondonSquad-PlanMate"
+    private const val projectsCollectionName = "projects"
+    private const val tasksCollectionName = "tasks"
+    private const val taskStatesCollectionName = "task_states"
+    private const val logsCollectionName = "logs"
+
+    private val client = MongoClients.create("mongodb+srv://LondonSquad:MKMa6ETmaNz8Asnl@cluster0.qibabiq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    private val database: MongoDatabase = client.getDatabase(dbName)
+    private val projectsCollection: MongoCollection<Document> = database.getCollection(projectsCollectionName)
+    private val tasksCollection: MongoCollection<Document> = database.getCollection(tasksCollectionName)
+    private val taskStatesCollection: MongoCollection<Document> = database.getCollection(taskStatesCollectionName)
+    private val logsCollection: MongoCollection<Document> = database.getCollection(logsCollectionName)
+
+    fun getProjectCollection(): MongoCollection<Document> = projectsCollection
+    fun getTasksCollection(): MongoCollection<Document> = tasksCollection
+    fun getTaskStatesCollection(): MongoCollection<Document> = taskStatesCollection
+    fun getLogsCollection(): MongoCollection<Document> = logsCollection
+}
