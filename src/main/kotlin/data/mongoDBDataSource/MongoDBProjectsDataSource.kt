800ae390f77b21f01f2dad322d8c944480a30893
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-06 15:07:01 +0300

    add MongoDBProjectsDataSource implementation

--- src/main/kotlin/data/mongoDBDataSource/MongoDBProjectsDataSource.kt
+++ src/main/kotlin/data/mongoDBDataSource/MongoDBProjectsDataSource.kt
@@ -1,32 +1,45 @@
 package data.mongoDBDataSource
 
-import logic.entities.Project
-import logic.repositories.ProjectsRepository
+import com.mongodb.client.MongoCollection
+import data.dataSources.ProjectsDataSource
+import data.dto.ProjectDto
+import org.bson.Document
 import java.util.*
 
-class MongoDBProjectsDataSource() : ProjectsRepository {
-    override fun getAllProjects(includeDeleted: Boolean): List<Project> {
-        TODO("Not yet implemented")
+class MongoDBProjectsDataSource(
+    private val collection: MongoCollection<Document>
+) : ProjectsDataSource {
+    override fun getAllProjects(): List<ProjectDto> {
+        return collection.find().map { doc ->
+            ProjectDto(
+                id = UUID.fromString(doc.getString(ID_FIELD)),
+                title = doc.getString(TITLE_FIELD),
+                description = doc.getString(DESCRIPTION_FIELD),
+                isDeleted = doc.getBoolean(IS_DELETED_FIELD)
+            )
+        }.toList()
     }
 
-    override fun getProjectById(projectId: UUID, includeDeleted: Boolean): Project {
-        TODO("Not yet implemented")
-    }
-
-    override fun addNewProject(project: Project) {
-        TODO("Not yet implemented")
+    override fun addNewProject(project: ProjectDto) {
+        val doc = Document(ID_FIELD, project.id.toString())
+            .append(TITLE_FIELD, project.title)
+            .append(DESCRIPTION_FIELD, project.description)
+            .append(IS_DELETED_FIELD, project.isDeleted)
+        collection.insertOne(doc)
     }
 
     override fun editProjectTitle(projectId: UUID, newTitle: String) {
-        TODO("Not yet implemented")
+        val updateDoc = Document("\$set", Document(TITLE_FIELD, newTitle))
+        collection.updateOne(Document(ID_FIELD, projectId.toString()), updateDoc)
     }
 
     override fun editProjectDescription(projectId: UUID, newDescription: String) {
-        TODO("Not yet implemented")
+        val updateDoc = Document("\$set", Document(DESCRIPTION_FIELD, newDescription))
+        collection.updateOne(Document(ID_FIELD, projectId.toString()), updateDoc)
     }
 
     override fun deleteProject(projectId: UUID) {
-        TODO("Not yet implemented")
+        collection.deleteOne(Document(ID_FIELD, projectId.toString()))
     }
 
     companion object {
