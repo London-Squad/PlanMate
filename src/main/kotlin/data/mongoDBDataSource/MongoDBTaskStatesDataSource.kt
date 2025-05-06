3758aa3611471304711ad8a8d3eb52e60ee329b7
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-06 15:31:41 +0300

    add MongoDBTaskStatesDataSource implementation

--- src/main/kotlin/data/mongoDBDataSource/MongoDBTaskStatesDataSource.kt
+++ src/main/kotlin/data/mongoDBDataSource/MongoDBTaskStatesDataSource.kt
@@ -1,29 +1,48 @@
 package data.mongoDBDataSource
 
+import com.mongodb.client.MongoCollection
+import org.bson.Document
+import java.util.*
 import data.dataSources.TasksStatesDataSource
 import data.dto.TaskStateDto
-import java.util.*
 
-class MongoDBTaskStatesDataSource : TasksStatesDataSource {
+class MongoDBTaskStatesDataSource(
+    private val collection: MongoCollection<Document>
+) : TasksStatesDataSource {
 
     override fun getAllTasksStates(): List<TaskStateDto> {
-        TODO("Not yet implemented")
+            return collection.find().map { doc ->
+                TaskStateDto(
+                    id = UUID.fromString(doc.getString(ID_FIELD)),
+                    title = doc.getString(TITLE_FIELD),
+                    description = doc.getString(DESCRIPTION_FIELD),
+                    projectId = UUID.fromString(doc.getString(PROJECT_ID_FIELD)),
+                    isDeleted = doc.getBoolean(IS_DELETED_FIELD)
+                )
+            }.toList()
     }
 
     override fun addNewTaskState(taskStateDto: TaskStateDto) {
-        TODO("Not yet implemented")
+        val doc = Document(ID_FIELD, taskStateDto.id.toString())
+            .append(TITLE_FIELD, taskStateDto.title)
+            .append(DESCRIPTION_FIELD, taskStateDto.description)
+            .append(PROJECT_ID_FIELD, taskStateDto.projectId.toString())
+            .append(IS_DELETED_FIELD, taskStateDto.isDeleted)
+        collection.insertOne(doc)
     }
 
     override fun editTaskStateTitle(stateId: UUID, newTitle: String) {
-        TODO("Not yet implemented")
+        val updateDoc = Document("\$set", Document(TITLE_FIELD, newTitle))
+        collection.updateOne(Document(ID_FIELD, stateId.toString()), updateDoc)
     }
 
     override fun editTaskStateDescription(stateId: UUID, newDescription: String) {
-        TODO("Not yet implemented")
+        val updateDoc = Document("\$set", Document(DESCRIPTION_FIELD, newDescription))
+        collection.updateOne(Document(ID_FIELD, stateId.toString()), updateDoc)
     }
 
     override fun deleteTaskState(stateId: UUID) {
-        TODO("Not yet implemented")
+            collection.deleteOne(Document(ID_FIELD, stateId.toString()))
     }
 
     companion object {
