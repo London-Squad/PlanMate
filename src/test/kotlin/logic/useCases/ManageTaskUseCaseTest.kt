c5197abfcd4d2ab3a988c5ee47d9801270604922
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-08 17:21:22 +0300

    fix the current test for ManageTaskUseCaseTest

--- src/test/kotlin/logic/useCases/ManageTaskUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/ManageTaskUseCaseTest.kt
@@ -1,60 +1,63 @@
-//package logic.useCases
-//
-//import io.mockk.mockk
-//import io.mockk.verify
-//import logic.entities.State
-//import logic.entities.Task
-//import logic.repositories.TaskRepository
-//import org.junit.jupiter.api.BeforeEach
-//import org.junit.jupiter.api.Test
-//import java.util.UUID
-//
-//class ManageTaskUseCaseTest {
-//
-//    private lateinit var taskRepository: TaskRepository
-//    private lateinit var manageTaskUsecase: ManageTaskUseCase
-//
-//    @BeforeEach
-//    fun setup() {
-//        taskRepository = mockk(relaxed = true)
-//        manageTaskUsecase = ManageTaskUseCase(taskRepository)
-//    }
-//
-//    private val task = Task(UUID.randomUUID(), "test", description = "description")
-//
-//    @Test
-//    fun `editTaskTitle should call taskRepository editTaskTitle`() {
-//
-//        manageTaskUsecase.editTaskTitle(task.id, "new title")
-//
-//        verify(exactly = 1) { taskRepository.editTaskTitle(task.id, "new title") }
-//    }
-//
-//    @Test
-//    fun `editTaskDescription should call taskRepository editTaskDescription`() {
-//
-//        manageTaskUsecase.editTaskDescription(task.id, "new description")
-//
-//        verify(exactly = 1) { taskRepository.editTaskDescription(task.id, "new description") }
-//    }
-//
-//    @Test
-//    fun `editTaskState should call taskRepository editTaskState`() {
-//        val state = State(UUID.randomUUID(), title = "FakeState", description = "")
-//
-//        manageTaskUsecase.editTaskState(task.id, state)
-//
-//        verify(exactly = 1) { taskRepository.editTaskState(task.id, state) }
-//    }
-//
-//
-//    @Test
-//    fun `deleteTask should call taskRepository deleteTask`() {
-//
-//        manageTaskUsecase.deleteTask(task.id)
-//
-//        verify(exactly = 1) { taskRepository.deleteTask(task.id) }
-//    }
-//
-//
-//}
+package logic.useCases
+
+import io.mockk.mockk
+import io.mockk.verify
+import logic.entities.Task
+import logic.entities.TaskState
+import logic.repositories.ProjectsRepository
+import logic.repositories.TaskRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class ManageTaskUseCaseTest {
+
+    private lateinit var taskRepository: TaskRepository
+    private lateinit var manageTaskUseCase: ManageTaskUseCase
+    private lateinit var createLogUseCase: CreateLogUseCase
+    private lateinit var projectsRepository: ProjectsRepository
+
+    @BeforeEach
+    fun setup() {
+        taskRepository = mockk(relaxed = true)
+        createLogUseCase = mockk(relaxed = true)
+        projectsRepository = mockk(relaxed = true)
+        manageTaskUseCase = ManageTaskUseCase(taskRepository, createLogUseCase, projectsRepository)
+    }
+
+    private val task = Task(UUID.randomUUID(), "test", description = "description")
+
+    @Test
+    fun `editTaskTitle should call taskRepository editTaskTitle`() {
+
+        manageTaskUseCase.editTaskTitle(task.id, "new title")
+
+        verify(exactly = 1) { taskRepository.editTaskTitle(task.id, "new title") }
+    }
+
+    @Test
+    fun `editTaskDescription should call taskRepository editTaskDescription`() {
+
+        manageTaskUseCase.editTaskDescription(task.id, "new description")
+
+        verify(exactly = 1) { taskRepository.editTaskDescription(task.id, "new description") }
+    }
+
+    @Test
+    fun `editTaskState should call taskRepository editTaskState`() {
+        val state = TaskState(UUID.randomUUID(), title = "FakeState", description = "")
+
+        manageTaskUseCase.editTaskState(task.id, state)
+
+        verify(exactly = 1) { taskRepository.editTaskState(task.id, state) }
+    }
+
+
+    @Test
+    fun `deleteTask should call taskRepository deleteTask`() {
+
+        manageTaskUseCase.deleteTask(task.id)
+
+        verify(exactly = 1) { taskRepository.deleteTask(task.id) }
+    }
+}
