a8d12ea2d4b5dead3c2e8fb8badfc30417539109
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-05-15 16:44:26 +0300

    achieve full coverage on ManageStateUseCase

--- src/test/kotlin/ui/taskManagementView/FakeProjectData.kt
+++ src/test/kotlin/ui/taskManagementView/FakeProjectData.kt
@@ -9,15 +9,16 @@ object FakeProjectData {
     val project = Project(
         UUID.randomUUID(), "Fake Project", "description",
     )
-    val taskStatesLists = listOf(
+
+    val taskStatesList = listOf(
         TaskState(UUID.randomUUID(), "Todo", "Todo description", project.id),
         TaskState(UUID.randomUUID(), "Done", "Done description", project.id),
         TaskState(UUID.randomUUID(), "In-progress", "In-progress description", project.id),
     )
 
-    val tasks = listOf(
-        Task(UUID.randomUUID(), "Fake Task 1", description = "description1", taskStatesLists[0].id),
-        Task(UUID.randomUUID(), "Fake Task 2", description = "description2", taskStatesLists[1].id),
-        Task(UUID.randomUUID(), "Fake Task 3", description = "description3", taskStatesLists[2].id),
+    val tasksList = listOf(
+        Task(UUID.randomUUID(), "Fake Task 1", description = "description1", taskStatesList[0].id),
+        Task(UUID.randomUUID(), "Fake Task 2", description = "description2", taskStatesList[1].id),
+        Task(UUID.randomUUID(), "Fake Task 3", description = "description3", taskStatesList[2].id),
     )
 }
